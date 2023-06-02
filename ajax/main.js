ec69de4496e6c80bd19f253cd6253b2d398f7b94
Olivier PERDRIX <olivierperdrix@live.fr>
2023-06-02 08:57:04 +0200

    Amélioration du code

--- ajax/main.js
+++ ajax/main.js
@@ -5,29 +5,87 @@ const result = document.querySelector('#target')
 
 btnRun.addEventListener('click', submitForm)
 
+/**
+ * Rajoute le HTML dans la div#target
+ * @param {String} html 
+ */
+function insertHTML(html) {
+  result.innerHTML = html
+}
+
+/**
+ * 
+ * Génère le html pour afficher les contacts
+ *
+ * @param {Contacts[]} contacts
+ * @returns {void}
+ */
+function displayContact(contacts) {
+  let html = `<ul>`
+  contacts.forEach(contact => {
+    html += `
+      <li>
+        <strong>Prénom :</strong> ${contact.firstname}<br>
+        <em>Téléphone :</em> ${contact.phone}<br>
+      </li>`
+  })
+  html += `</ul>`
+  insertHTML(html)
+}
+
+/**
+ * Génère le HTML qui affiche la liste de films
+ * @param {Movies[]} movies
+ */
+function displayMovies(movies) {
+  let html = "<ul class='movie-list'>";
+  movies.forEach(
+    (movie) =>
+    (html += `
+				<li>
+					<img src="images/${movie.cover}">
+					<p>
+						<strong>${movie.title}</strong> -
+						<em>${movie.duration}</em>
+					</p>
+				</li>
+			`)
+  );
+  html += "</ul>";
+  insertHTML(html);
+}
+/**
+ * Exécute l'appel AJAX
+ * @param {string} url
+ * @param {function} fctCallback
+ * @param {boolean} json par défaut à `false`
+ */
+function ajax(file, fctCallback, json = false) {
+  fetch(file)
+    .then((response) => (json ? response.json() : response.text()))
+    .then((datas) => fctCallback(datas));
+}
+
+/**
+ * Soumet le formulaire
+ * @returns {void}
+ */
 function submitForm() {
   const what = parseInt(form.querySelector('input[name=what]:checked').value)
-  let file = ''
   switch (what) {
     case 1:
-      file = 'data/1-get-html-article.html'
+      ajax("data/1-get-html-article.html", insertHTML)
       break
     case 2:
-      file = 'data/2-get-contacts-list.json'
+      ajax("data/2-get-contacts-list.json", displayContact, true)
       break
     case 3:
-      file = 'data/3-get-html-movies.html'
+      ajax("data/3-get-html-movies.html", insertHTML)
       break
     case 4:
-      file = 'data/4-get-json-movies.json'
+      ajax("data/4-get-json-movies.json", displayMovies, true)
       break
     default:
       throw new Error('Invalid value')
   }
-  fetch(file)
-    .then(response => response.text())
-    .then(data => {
-      result.innerHTML = data
-    })
-    .catch(error => console.error(error))
 }
